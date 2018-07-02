03d9979b1ab17b81336d8bb63e8566aeaf61516f
mr.ebola <me@mrebola.com>
2018-07-01 22:14:21 -0500

    first version of bash parser

--- BASH/parser/bashparser.sh
+++ BASH/parser/bashparser.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-mongo --eval 'db.leaks.insert({correo:"test@test.com",passwd:"pass"});'
+dirs=(dummydata dummydata2 dummydata3) #Directories to iterate
+for dir in "${dirs[@]}"; do
+    echo "PROCESANDO DIRECTORIO: $dir"
+    	while read -r line
+		do
+		    IFS=:
+		    set - $line
+			echo "Correo: $1 Pass: $2" 
+			echo "db.leaks.insert({correo:\"$1\",passwd:\"$2\"});" | mongo
+		done < "$dir"
+done
