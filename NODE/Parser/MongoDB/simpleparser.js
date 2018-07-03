27e7bd4ca252a3f9aa134456ba50144beaea24e3
mr.ebola <me@mrebola.com>
2018-07-03 00:49:54 -0500

    adding arrays to simple parser

--- NODE/Parser/MongoDB/simpleparser.js
+++ NODE/Parser/MongoDB/simpleparser.js
@@ -1,19 +1,18 @@
 // Simple Parser by mr.ebola
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
+var files = [ 'file.in', 'test.in'];
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("leaksmongo"); 
-  
   var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('file.in')
+  
+  input: require('fs').createReadStream(files[0])
   });
   lineReader.on('line', function (line) {
-  var myarr = line.split(":");
-  //console.log("user:" + myarr[0] + " pass: " + myarr[1]);
+    var myarr = line.split(":");
     var myobj = { mail:  myarr[0] , pass:  myarr[1] };
-
   dbo.collection("leaksdb").insertOne(myobj, function(err, res) {
     if (err) throw err;
     console.log("1 document inserted");
